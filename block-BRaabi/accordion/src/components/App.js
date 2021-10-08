1a464525a73b9db2febb75e847904f826ff85f64
bitopan987 <bitopanxon1@gmail.com>
2021-10-09 01:16:43 +0530

    refactor

--- block-BRaabi/accordion/src/components/App.js
+++ block-BRaabi/accordion/src/components/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import faqs from './data.json';
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeIndex: null,
+    };
+  }
   render() {
     return (
       <>
-        <h1>Happy Coding</h1>
+        <div className="container">
+          <h1>AltCampus FAQ</h1>
+          <ul>
+            {faqs.map((faq, index) => {
+              return (
+                <li>
+                  <h2
+                    onClick={() => {
+                      this.setState({
+                        activeIndex:
+                          this.state.activeIndex === index ? null : index,
+                      });
+                    }}
+                  >
+                    {faq.Q} {this.state.activeIndex === index ? '👆️' : '👇️'}
+                  </h2>
+                  {index === this.state.activeIndex && <p>{faq.A}</p>}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </>
     );
   }
