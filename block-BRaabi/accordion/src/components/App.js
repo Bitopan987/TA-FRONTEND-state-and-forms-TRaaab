9e3401a12e58fefca566225886f13f3a0bcfb35d
bitopan987 <bitopanxon1@gmail.com>
2021-10-08 20:58:42 +0530

    accordion

---
+++ block-BRaabi/accordion/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Happy Coding</h1>
+      </>
+    );
+  }
+}
+
+export default App;
